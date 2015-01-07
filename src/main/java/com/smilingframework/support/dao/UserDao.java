4e9c35a3d35b1c6c88b04ad1db38ca5c8c735072
xiaoxin <392931818@qq.com>
2015-01-07 20:47:52 +0800

    update

--- src/main/java/com/smilingframework/support/dao/UserDao.java
+++ src/main/java/com/smilingframework/support/dao/UserDao.java
@@ -1,6 +1,6 @@
 package com.smilingframework.support.dao;
 
-import com.smilingframework.dao.dal.BaseJpaRepository;
+import com.smilingframework.dao.base.BaseJpaRepository;
 import com.smilingframework.support.model.User;
 
 public interface UserDao extends BaseJpaRepository<User, String> {
